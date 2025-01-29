8fee1da8ceb04c7649165bac5c5e80d87dd91300
Sergey <longsendoff@gmail.com>
2025-01-30 01:05:48 +0500

    Add files via upload

--- dvmn_task_1.3/dvmn-task-1.3.py
+++ dvmn_task_1.3/dvmn-task-1.3.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 import smtplib
+
+sender_email = 'devmanorg@yandex.ru' # арес отправителя
+recipient_email = 'iCloudCD@yandex.ru' # адрес получателя
+theme = 'Приглашение!' # тема письма
+my_name = 'Сергей' # имя отправителя
+friend_name = 'Павел' # имя получателя
+web_site = 'https://dvmn.org/profession-ref-program/id77480752/InhEP/' # имя сайта
 
 letter = """\
 From: {from_who}
 To: {to_who}
-Subject: {theme}
+Subject: {theme_of_letter}
 
 Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
 
@@ -24,11 +30,12 @@ Subject: {theme}
 Все проекты — они же решение наших задачек — можно разместить на твоём Gitub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".format(from_who='devmanorg@yandex.ru', to_who='iCloudCD@yandex.ru', theme='Приглашение')
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".format(\
+from_who=sender_email, to_who=recipient_email, theme_of_letter=theme)
 
-letter = letter.replace('%website%', 'https://dvmn.org/profession-ref-program/id77480752/InhEP/') # имя сайта
-letter = letter.replace('%friend_name%', 'Павел') # имя друга
-letter = letter.replace('%my_name%', 'Сергей') # имя отправителя
+letter = letter.replace('%my_name%', my_name)
+letter = letter.replace('%friend_name%', friend_name)
+letter = letter.replace('%website%', web_site)
 
 letter = letter.encode("UTF-8")
 
@@ -37,7 +44,5 @@ TOKEN = os.getenv('MY_TOKEN')
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465) 
 server.login(LOGIN, TOKEN)
-server.sendmail('devmanorg@yandex.ru', 'iCloudCD@yandex.ru', letter)
+server.sendmail(sender_email, recipient_email, letter)
 server.quit()
-
-print(letter)
